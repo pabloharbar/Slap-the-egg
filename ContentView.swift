3ae24d0ce0a2ad41ee9dd9eb8a9a98e77665c0a8
Pablo <harbar2705@gmail.com>
2022-01-27 17:07:50 -0300

    Projeto incializado

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slap the egg
+//
+//  Created by Pablo Penas on 27/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slap_the_eggApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slap_the_eggApp.swift
+//  Slap the egg
+//
+//  Created by Pablo Penas on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Slap_the_eggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
