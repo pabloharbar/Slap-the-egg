f224644deda548d5f9a720f9022c6da08f29baba
Pablo <harbar2705@gmail.com>
2022-02-03 14:20:31 -0300

    calibraçao do movimento da panela

--- Slap the egg/Scenes/Game/Model/Pan.swift
+++ Slap the egg/Scenes/Game/Model/Pan.swift
@@ -17,14 +17,14 @@ class Pan {
     
     func zoomIn() {
         let zoomIn = SKAction.scale(to: 3, duration: 1)
-        let move = SKAction.moveBy(x: -50, y: -200, duration: 1)
+        let move = SKAction.moveBy(x: -50, y: -300, duration: 1)
         node.run(zoomIn)
         node.run(move)
     }
     
     func zoomOut() {
         let zoomOut = SKAction.scale(to: 1.8, duration: 1)
-        let move = SKAction.moveBy(x: 50, y: 200, duration: 1)
+        let move = SKAction.moveBy(x: 50, y: 300, duration: 1)
         node.run(zoomOut)
         node.run(move)
     }
