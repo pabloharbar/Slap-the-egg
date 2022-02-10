59595c209a477915e65509ec4c1eedd420dc2fc3
luisa <luisabtrabulci@gmail.com>
2022-02-10 18:39:20 -0300

    update haptics

--- Slap the egg/Scenes/Game/View Model/HapticManager.swift
+++ Slap the egg/Scenes/Game/View Model/HapticManager.swift
@@ -24,20 +24,10 @@ class HapticsManager {
 struct HapticManager: View {
     var body: some View {
         VStack(spacing: 20) {
-            Button("success") { HapticsManager.instance.notification(type: .success) }
             Button("error") { HapticsManager.instance.notification(type: .error) }
-            Button("warning") { HapticsManager.instance.notification(type: .warning) }
             Divider()
             Button("soft") {
                 HapticsManager.instance.impact(style: .soft) }
-            Button("light") {
-                HapticsManager.instance.impact(style: .light) }
-            Button("medium") {
-                HapticsManager.instance.impact(style: .medium) }
-            Button("rigid") {
-                HapticsManager.instance.impact(style: .rigid) }
-            Button("heavy") {
-                HapticsManager.instance.impact(style: .heavy) }
         }
     }
 }
